34f048b585507253a5d670fd209720609caea9f4
ekapus <ed.kapuscinski@berndtgroup.net>
2013-11-28 14:35:13 -0500

    Created Cars class

---
+++ WindsorSt/Models/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WindsorSt.Models
+{
+    public class WindsorStDBContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+
+    public class Car
+    {
+        public String Owner { get; set; }
+        public String ReportingMarks { get; set; }
+        public String Color { get; set; }
+        public String PaintSchemeNotes { get; set; }
+        public String RoadNumber { get; set; }
+        public String CTD { get; set; }
+        public String Blt { get; set; }
+        public String Srvc { get; set; }
+        public String Length { get; set; }
+        public String GeneralCarType { get; set; }
+        public String SpecificCarType { get; set; }
+        public String Service { get; set; }
+        public String Manufacturer { get; set; }
+        public String Item { get; set; }
+        public String Weight { get; set; }
+        public String Wheels { get; set; }
+        public String Trucks { get; set; }
+        public String Couplers { get; set; }
+        public String Weathered { get; set; }
+        public String AcquisitionDate { get; set; }
+        public String PricePaid { get; set; }
+        public String RepairsNeeded { get; set; }
+        public String OnLayoutDestination { get; set; }
+        public String OffLayoutDestination { get; set; } 
+    }
+}

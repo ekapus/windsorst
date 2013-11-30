845fdaee0234a0e1199f0b0f993bd9873ce2e468
ekapus <ed.kapuscinski@berndtgroup.net>
2013-11-30 11:43:43 -0500

    WIP: Block

---
+++ WindsorSt/Models/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WindsorSt.Models
+{
+    public class Block
+    {
+        public int Id { get; set; }
+        public Train HandlingTrain { get; set; }
+        public string Name { get; set; }
+    }
+}

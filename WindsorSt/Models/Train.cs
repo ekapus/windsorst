5d1b1a8cde6ace313d25258e6fd78bd62b88fe7f
ekapus <ed.kapuscinski@berndtgroup.net>
2013-11-30 11:37:31 -0500

    WIP: Train class

---
+++ WindsorSt/Models/Train.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WindsorSt.Models
+{
+    public class Train
+    {
+        public int Id { get; set; }
+        public String Symbol {get; set;}
+        public String TrainType {get; set;}
+        public String Description { get; set; }
+    }
+}

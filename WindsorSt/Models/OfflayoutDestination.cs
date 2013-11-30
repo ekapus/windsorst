72c1c0468b2efd370519d8db5d53cc3a70dccb9f
ekapus <ed.kapuscinski@berndtgroup.net>
2013-11-30 11:46:44 -0500

    WIP: Offlayout Destination

---
+++ WindsorSt/Models/OfflayoutDestination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WindsorSt.Models
+{
+    public class OfflayoutDestination
+    {
+        public int Id { get; set; }
+        public Block Block { get; set; }
+        public string Name { get; set; }
+    }
+}
